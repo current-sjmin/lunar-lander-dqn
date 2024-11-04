5566f5fb0498a42c526aa429fda8fed2b010fdef
sjmin <sjmin@swm.ai>
2024-11-04 17:17:47 +0900

    add the code for prototype

---
+++ README.md
@@ -0,0 +1,24 @@
+# LunarLanderDQN
+
+## Building a Development Environment
+### - Create the venv
+* python -m venv venv
+* source venv/bin/activate -> (ubuntu)
+* .\venv\Scripts\activate -> (window)
+* pip install --upgrade pip
+
+### - If you use ubuntu OS
+* sudo apt-get install swig
+* sudo apt-get install python3-dev
+* pip install -r requirements.txt
+
+### - If you use window OS
+* Install the Microsoft C++ Build Tools ([Link](https://visualstudio.microsoft.com/ko/visual-cpp-build-tools/))
+* Install the Swig ([Link](https://sourceforge.net/projects/swig/files/swigwin/swigwin-3.0.2/swigwin-3.0.2.zip/download)) -> (set the system path)
+* pip install -r requirements.txt
+
+### Train the LunarLander.
+* python main.py
+
+### Test the LunarLander.
+* python test.py
---
+++ test.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import gym
+
+
+class QNetwork(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_size, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, action_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        return self.fc3(x)
+
+env = gym.make("LunarLander-v2", render_mode='human')
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.n
+
+loaded_model = QNetwork(state_size=state_size, action_size=action_size)
+loaded_model.load_state_dict(torch.load("./results/models/best_reward_model.pth"))
+loaded_model.eval()
+
+while True:
+    state, data_type = env.reset()
+    done = False
+    total_reward = 0
+    while not done:
+        state_tensor = torch.FloatTensor(state).unsqueeze(0)
+
+        with torch.no_grad():
+            q_values = loaded_model(state_tensor)
+        action = torch.argmax(q_values).item()
+        next_state, reward, done, truncated, info = env.step(action=action)
+        done = done or truncated
+        total_reward += reward
+        state = next_state
